e4d789911efa29625127af2e90a7947a05d02c88
aryan.pourranjbar2 <aryan.pourranjbar2@600900B211DE403>
2018-04-04 11:59:38 +1000

    new Project

---
+++ Assets/~Minesweeper2D/Scripts/Tile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Minesweeper2D
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Tile : MonoBehaviour
+    {
+
+        public int x = 0;
+        public int y = 0;
+        public bool isMine = false;
+        public bool isRevealed = false;
+        [Header("References")]
+        public Sprite[] emptySprites;
+        public Sprite[] mineSprites;
+        private SpriteRenderer rend;
+        // Use this for initialization
+        void Awake()
+        {
+           rend = GetComponent<SpriteRenderer>();
+        }
+        void Start()
+        {
+            isMine = Random.value < .05f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        public void Reveal(int adjacentMine, int mineState = 0)
+        {
+            isRevealed = true;
+            if(isMine)
+            {
+                rend.sprite = emptySprites[adjacentMine];
+            }
+        }
+    }
+}
